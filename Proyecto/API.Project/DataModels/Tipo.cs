0f57bc27f1aa086c6141bd35611acb46f4ba4c91
AndresMG0726 <andresmora.g10@gmail.com>
2022-03-20 11:24:54 -0600

    actualizaciones
    mapping profile, strings conexion

---
+++ Proyecto/API.Project/DataModels/Tipo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace API.DataModels
+{
+    public partial class Tipo
+    {
+        public Tipo()
+        {
+            Solicitud = new HashSet<Solicitud>();
+        }
+
+        public int IdTipo { get; set; }
+        public string DescripcionTipo { get; set; }
+
+        public virtual ICollection<Solicitud> Solicitud { get; set; }
+    }
+}
---
+++ Proyecto/API.Project/Mapping/MappingProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using data = DAL.DO.Objects;
+
+
+namespace API.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<data.Departamento, DataModels.Departamento>().ReverseMap();
+            CreateMap<data.Estado, DataModels.Estado>().ReverseMap();
+            CreateMap<data.HorasExtra, DataModels.HorasExtra>().ReverseMap();
+            CreateMap<data.InfoContactoUsuario, DataModels.InfoContactoUsuario>().ReverseMap();
+            CreateMap<data.Job, DataModels.Job>().ReverseMap();
+            CreateMap<data.Rol, DataModels.Rol>().ReverseMap();
+            CreateMap<data.RolContacto, DataModels.RolContacto>().ReverseMap();
+            CreateMap<data.Solicitud, DataModels.Solicitud>().ReverseMap();
+            CreateMap<data.Tipo, DataModels.Tipo>().ReverseMap();
+            CreateMap<data.Usuario, DataModels.Usuario>().ReverseMap();
+        }
+    }
+}
